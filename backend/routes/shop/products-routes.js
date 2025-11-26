369274fe6e12845efc6ec5fe2aef88544e23e163
shahil-07 <shahilullal432@gmail.com>
2025-11-26 17:36:49 +0530

    modal

--- backend/routes/shop/products-routes.js
+++ backend/routes/shop/products-routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 
 const {
-getFilteredProducts
+getFilteredProducts,
+getProductDetails
 } = require("../../controllers/shop/products-controller");
 
 const { upload } = require("../../helpers/cloudinary");
 
 const router = express.Router();
 router.get('/get', getFilteredProducts)
+router.get('/get/:id', getProductDetails)
 
 module.exports = router;
